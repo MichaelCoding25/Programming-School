76a65bfc66cb09bfe5f3e2e79d30251309b7f1a8
Michael Spector <43636601+MichaelCoding25@users.noreply.github.com>
2020-10-01 09:00:18 +0300

    do stuff v2

--- Assembly/C/TOOLS/start.bat
+++ Assembly/C/TOOLS/start.bat
@@ -1,1 +1,1 @@
-start "DOSBOX" "C:\Program Files (x86)\DOSBox-0.74-3\dosbox.exe" -conf C:/Users/mikis/Desktop/Coding/Assembly/C/TOOLS/conf.conf
+start "DOSBOX" "C:\Program Files (x86)\DOSBox-0.74-3\dosbox.exe" -conf C:\Users\mikis\OneDrive\Desktop\Coding\Assembly\C\TOOLS\conf.conf
