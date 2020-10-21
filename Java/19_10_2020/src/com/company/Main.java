1e961f9f9488944e5c41b49d585d9aefa8623347
Michael Spector <43636601+MichaelCoding25@users.noreply.github.com>
2020-10-21 10:10:00 +0300

    Ex0 Java

---
+++ Java/19_10_2020/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int c = 31;
+        int f = (c * 9/5) + 32;
+        System.out.println(f);
+
+    }
+}
