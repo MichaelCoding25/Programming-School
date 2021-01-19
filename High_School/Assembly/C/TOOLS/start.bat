99973b1b73277e22619d0214a0c33a03343d05bd
Michael Spector <43636601+MichaelCoding25@users.noreply.github.com>
2021-01-19 12:59:07 +0200

    Update Assembly configuration

--- High_School/Assembly/C/TOOLS/start.bat
+++ High_School/Assembly/C/TOOLS/start.bat
@@ -1,1 +1,1 @@
-start "DOSBOX" "C:\Program Files (x86)\DOSBox-0.74-3\dosbox.exe" -conf C:\Users\mikis\OneDrive\Desktop\Coding\Assembly\C\TOOLS\conf.conf
+start "DOSBOX" "C:\Program Files (x86)\DOSBox-0.74-3\dosbox.exe" -conf E:\Coding\High_School\Assembly\C\TOOLS\conf.conf
